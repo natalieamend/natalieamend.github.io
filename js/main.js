b1045101c5d74431dece7a699845c512958d15fd
Natalie Amend <natalieroseamend@gmail.com>
2015-11-13 12:57:35 -0600

    media queries

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+(function($) {          
+    $(document).ready(function(){                    
+        $(window).scroll(function(){                          
+            if ($(this).scrollTop() > 200) {
+                $('#portfolio').fadeIn(500);
+            } else {
+                $('#portfolio').fadeOut(500);
+            }
+        });
+    });
+})(jQuery);
+
 $(".menu-toggle").on('click', function() {
   $(this).toggleClass("on");
   $('.menu-section').toggleClass("on");
